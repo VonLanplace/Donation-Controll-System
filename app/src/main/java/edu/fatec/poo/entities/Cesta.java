eba2b0d3ca992605730b5b04b33fe28ecc5708fc
Lucas Sartorelli <lucaspmsartorelli@gmail.com>
2026-05-02 16:15:36 -0300

    feature: entity for produto

---
+++ app/src/main/java/edu/fatec/poo/entities/Cesta.java
@@ -0,0 +1,21 @@
+package edu.fatec.poo.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cesta {
+    private Long id;
+    private LocalDate dataEntrega;
+
+    public boolean isEntreque() {
+        return dataEntrega != null;
+    }
+}
