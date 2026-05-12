c5494db90a5386200e88573f5f9899b8116f5c8a
Lucas Sartorelli <lucaspmsartorelli@gmail.com>
2026-05-11 22:34:30 -0300

    temp: start of Residencia Fields

--- app/src/main/java/edu/fatec/poo/views/UICadastrarPessoa.java
+++ app/src/main/java/edu/fatec/poo/views/UICadastrarPessoa.java
@@ -8,9 +8,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import lombok.NoArgsConstructor;
@@ -34,11 +32,10 @@ public class UICadastrarPessoa extends Application {
         Tab tabPessoal = new Tab("Dados Pessoais", buildPaneDadosPessoais());
         tabPessoal.setClosable(false);
 
-        for (Tab t : tabPane.getTabs()) {
-            t.setClosable(false);
-        }
+        Tab tabResidencia = new Tab("Dados Residênciais", buildPaneDadosResidenciais());
+        tabResidencia.setClosable(false);
 
-        tabPane.getTabs().addAll(tabPessoal);
+        tabPane.getTabs().addAll(tabPessoal, tabResidencia);
 
         Scene scene = new Scene(tabPane, WHIDTH, HEIGHT);
 
@@ -47,89 +44,125 @@ public class UICadastrarPessoa extends Application {
         primaryStage.show();
     }
 
+    private Node buildPaneDadosResidenciais() {
+        VBox paneDadosPessoais = new VBox();
+        paneDadosPessoais.setPadding(PADDING);
+        paneDadosPessoais.setSpacing(SPACING);
+
+        Label lblPessoal = new Label("Dados Residênciais");
+
+        GridPane paneResidencial = new GridPane();
+
+        paneResidencial.setVgap(SPACING);
+        paneResidencial.setHgap(SPACING);
+        paneResidencial.setPadding(new Insets(SPACING));
+
+        TextField txtRua = new TextField();
+        txtRua.setPromptText("Insira a Rua.");
+
+        TextField txtCep = new TextField();
+        txtCep.setPromptText("Insira o CEP.");
+
+        TextField txtCidade = new TextField();
+        txtCidade.setPromptText("Insira a Cidade.");
+
+        TextField txtBairo = new TextField();
+        txtBairo.setPromptText("Insira o Bairo.");
+
+        TextField txtNum = new TextField();
+        txtNum.setPromptText("Insira o Numero.");
+        //TODO
+        txtNum.setTextFormatter(new TextFormatter<>(change -> {
+            if (change.getControlNewText().matches("\\d*")) {
+                return change;
+            }
+            return null;
+        }));
+
+        TextField txtComplemento = new TextField();
+        txtComplemento.setPromptText("Insira o Complemento.");
+
+        paneResidencial.add(lblPessoal, 0, 0, 1, 1);
+        paneResidencial.add(txtRua, 0, 1, 1, 1);
+        paneResidencial.add(txtCep, 1, 1, 1, 1);
+        paneResidencial.add(txtCidade, 0, 2, 1, 1);
+        paneResidencial.add(txtBairo, 1, 2, 1, 1);
+        paneResidencial.add(txtNum, 0, 3, 1, 1);
+        paneResidencial.add(txtComplemento, 1, 3, 1, 1);
+
+        paneDadosPessoais.getChildren().addAll(paneResidencial);
+        return paneDadosPessoais;
+    }
+
     private Node buildPaneDadosPessoais() {
         VBox paneDadosPessoais = new VBox();
+        paneDadosPessoais.setPadding(PADDING);
         paneDadosPessoais.setSpacing(SPACING);
-        paneDadosPessoais.setPadding(new Insets(SPACING));
-
-        // --- CONFIGURAÇÃO DE COLUNAS PARA OS GRIDS ---
-        // Criamos duas restrições de 50% de largura cada
-        ColumnConstraints col50 = new ColumnConstraints();
-        col50.setPercentWidth(50);
-        col50.setHgrow(Priority.ALWAYS);
-
-        // --- GRID PESSOAL ---
-        GridPane panePessoal = new GridPane();
-        panePessoal.setVgap(SPACING);
-        panePessoal.setHgap(SPACING);
-        // Aplica as colunas de 50%
-        panePessoal.getColumnConstraints().addAll(col50, col50);
 
         Label lblPessoal = new Label("Dados");
 
+        GridPane panePessoal = new GridPane();
+
+        panePessoal.setVgap(SPACING);
+        panePessoal.setHgap(SPACING);
+        panePessoal.setPadding(new Insets(SPACING));
+
         TextField txtNome = new TextField();
         txtNome.setPromptText("Insira o Nome.");
-        txtNome.setMaxWidth(Double.MAX_VALUE); // Faz esticar na célula
 
         TextField txtSobrenome = new TextField();
         txtSobrenome.setPromptText("Insira o Sobrenome.");
-        txtSobrenome.setMaxWidth(Double.MAX_VALUE);
 
         TextField txtCpf = new TextField();
         txtCpf.setPromptText("Insira o Cpf.");
-        txtCpf.setMaxWidth(Double.MAX_VALUE);
 
         TextField txtRg = new TextField();
         txtRg.setPromptText("Insira o numero Rg.");
-        txtRg.setMaxWidth(Double.MAX_VALUE);
 
         DatePicker dtpNascimeto = new DatePicker();
         dtpNascimeto.setPromptText("Insira a Data de Nascimento.");
-        dtpNascimeto.setMaxWidth(Double.MAX_VALUE); // DatePicker também precisa disso
 
         CheckBox ckbLeEscreve = new CheckBox("Lê/Escreve");
 
-        panePessoal.add(lblPessoal, 0, 0, 2, 1); // Span de 2 colunas para o título
-        panePessoal.add(txtNome, 0, 1);
-        panePessoal.add(txtSobrenome, 1, 1);
-        panePessoal.add(txtCpf, 0, 2);
-        panePessoal.add(txtRg, 1, 2);
-        panePessoal.add(dtpNascimeto, 0, 3);
-        panePessoal.add(ckbLeEscreve, 1, 3);
+        panePessoal.add(lblPessoal, 0, 0, 1, 1);
+        panePessoal.add(txtNome, 0, 1, 1, 1);
+        panePessoal.add(txtSobrenome, 1, 1, 1, 1);
+        panePessoal.add(txtCpf, 0, 2, 1, 1);
+        panePessoal.add(txtRg, 1, 2, 1, 1);
+        panePessoal.add(dtpNascimeto, 0, 3, 1, 1);
+        panePessoal.add(ckbLeEscreve, 1, 3, 1, 1);
 
-        // --- GRID CONTATO ---
+        paneDadosPessoais.getChildren().addAll(panePessoal);
+
         GridPane paneContato = new GridPane();
+
+        paneContato.setPadding(PADDING);
         paneContato.setHgap(SPACING);
         paneContato.setVgap(SPACING);
-        // Reaproveita as mesmas restrições de coluna
-        paneContato.getColumnConstraints().addAll(col50, col50);
 
         Label lblContato = new Label("Contato");
 
         TextField txtTelefone = new TextField();
         txtTelefone.setPromptText("Insira o Telefone");
-        txtTelefone.setMaxWidth(Double.MAX_VALUE);
 
         TextField txtTelefoneContato = new TextField();
         txtTelefoneContato.setPromptText("Insira o Telefone para Contato");
-        txtTelefoneContato.setMaxWidth(Double.MAX_VALUE);
 
         TextField txtEmail = new TextField();
         txtEmail.setPromptText("Insira o Email caso tenha");
-        txtEmail.setMaxWidth(Double.MAX_VALUE);
 
         CheckBox ckbJuridico = new CheckBox("Necessita de Júridico");
+
         CheckBox ckbPsico = new CheckBox("Necessita de Psicológa");
 
-        paneContato.add(lblContato, 0, 0, 2, 1); // Span de 2 colunas
-        paneContato.add(txtTelefone, 0, 1);
-        paneContato.add(txtTelefoneContato, 1, 1);
-        paneContato.add(txtEmail, 0, 2, 2, 1); // Email ocupando as duas colunas
-        paneContato.add(ckbJuridico, 0, 3);
-        paneContato.add(ckbPsico, 1, 3);
+        paneContato.add(lblContato, 0, 0, 1, 1);
+        paneContato.add(txtTelefone, 0, 1, 1, 1);
+        paneContato.add(txtTelefoneContato, 1, 1, 1, 1);
+        paneContato.add(txtEmail, 0, 2, 2, 1);
+        paneContato.add(ckbJuridico, 0, 3, 1, 1);
+        paneContato.add(ckbPsico, 1, 3, 1, 1);
 
-        paneDadosPessoais.getChildren().addAll(panePessoal, paneContato);
-
+        paneDadosPessoais.getChildren().addAll(paneContato);
         return paneDadosPessoais;
     }
 }
