fbdf7616fbb4651112ed268bf0f60e3a30ae63cc
Lucas Sartorelli <lucaspmsartorelli@gmail.com>
2026-05-14 09:10:18 -0300

    feature: first admin auto created

--- app/src/main/java/edu/fatec/poo/persistence/mysql/mySqlCreateTable.java
+++ app/src/main/java/edu/fatec/poo/persistence/mysql/mySqlCreateTable.java
@@ -1,7 +1,7 @@
 package edu.fatec.poo.persistence.mysql;
 
-import edu.fatec.poo.persistence.ADaoConnection;
-import edu.fatec.poo.persistence.ICreateTable;
+import edu.fatec.poo.persistence.connection.ADaoConnection;
+import edu.fatec.poo.persistence.connection.ICreateTable;
 
 import java.sql.*;
 
@@ -36,15 +36,34 @@ public class mySqlCreateTable implements ICreateTable {
                     nome VARCHAR(100) NOT NULL,
                     email VARCHAR(100) UNIQUE NOT NULL,
                     senha VARCHAR(100) NOT NULL,
-                    cpf VARCHAR(100) UNIQUE,
+                    cpf VARCHAR(14) UNIQUE,
                     telefone VARCHAR(15)
                 );
                 """;
+        runStatementTabela(sql, "Usuario");
+        sql = """
+                INSERT INTO usuario (acesso, nome, email, senha, cpf, telefone)
+                VALUES (0, 'admin', 'admin', 'admin', '12148628704', '40028922');
+                """;
+        runStatementData(sql, "Usuario");
+    }
+
+    private void runStatementTabela(String sql, String nomeTabela) throws SQLException {
         try (PreparedStatement ps = c.prepareStatement(sql)) {
             ps.execute();
-            System.out.println("[MySQL] Tabela Usuario criada com sucesso ou já existente.");
+            System.out.println("[MySQL] Tabela " + nomeTabela + " criada com sucesso ou já existente.");
         } catch (SQLException e) {
-            System.err.println("[MySQL] Erro ao criar tabela Usuario no: " + e.getMessage());
+            System.err.println("[MySQL] Erro ao criar tabela " + nomeTabela + " no: " + e.getMessage());
+            throw e;
+        }
+    }
+
+    private void runStatementData(String sql, String nomeTabela) throws SQLException {
+        try (PreparedStatement ps = c.prepareStatement(sql)) {
+            ps.execute();
+            System.out.println("[MySQL] Dado inserido na Tabela " + nomeTabela + " com sucesso.");
+        } catch (SQLException e) {
+            System.err.println("[MySQL] Erro ao inserir na tabela " + nomeTabela + " no: " + e.getMessage());
             throw e;
         }
     }
