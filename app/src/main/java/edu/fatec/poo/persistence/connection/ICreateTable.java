ad9864e7db62adf5e3c2ca6142dadc3dca03fe10
Lucas Sartorelli <lucaspmsartorelli@gmail.com>
2026-05-15 08:02:33 -0300

    feature: tipo_produto table creation

--- app/src/main/java/edu/fatec/poo/persistence/connection/ICreateTable.java
+++ app/src/main/java/edu/fatec/poo/persistence/connection/ICreateTable.java
@@ -7,4 +7,6 @@ public interface ICreateTable {
     void createTableAll() throws SQLException;
 
     void createTabelUsuario() throws SQLException;
+
+    void createTabelTipoProduto() throws SQLException;
 }
--- app/src/main/java/edu/fatec/poo/persistence/mariaDb/mariadbCreateTable.java
+++ app/src/main/java/edu/fatec/poo/persistence/mariaDb/mariadbCreateTable.java
@@ -1,22 +1,23 @@
 package edu.fatec.poo.persistence.mariaDb;
 
-import edu.fatec.poo.persistence.connection.ADaoConnector;
 import edu.fatec.poo.persistence.connection.ICreateTable;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class mariadbCreateTable implements ICreateTable {
 
     private final Connection connection;
 
-    public mariadbCreateTable(Connection connection) throws SQLException, ClassNotFoundException {
+    public mariadbCreateTable(Connection connection) {
         this.connection = connection;
     }
 
     @Override
     public void createTableAll() throws SQLException {
         createTabelUsuario();
-        //TODO
+        createTabelTipoProduto();
     }
 
     @Override
@@ -43,7 +44,7 @@ public class mariadbCreateTable implements ICreateTable {
         runStatementData(sql, "Usuario");
         sql = """
                 INSERT INTO usuario (acesso, nome, email, senha, cpf, telefone)
-                SELECT 1, 'user', 'user', 'user', '098765432101', '40028922'
+                SELECT 1, 'user', 'user', 'user', '09876543210', '40028922'
                 WHERE NOT EXISTS (
                     SELECT 1 FROM usuario WHERE acesso = 1
                 );
@@ -51,8 +52,41 @@ public class mariadbCreateTable implements ICreateTable {
         runStatementData(sql, "Usuario");
     }
 
+    @Override
+    public void createTabelTipoProduto() throws SQLException {
+        String nomeTabela = "tipo_produto";
+        String sql = """
+                CREATE TABLE IF NOT EXISTS tipo_produto(
+                    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                    nome VARCHAR(100) NOT NULL,
+                );
+                """;
+        runStatementTabela(sql, nomeTabela);
+        String slq = """
+                INSERT INTO tipo_produto (nome)
+                SELECT * FROM (
+                    SELECT 'Arroz' AS nome UNION ALL
+                    SELECT 'Feijão' UNION ALL
+                    SELECT 'Macarrão' UNION ALL
+                    SELECT 'Sabão em barra' UNION ALL
+                    SELECT 'Detergente' UNION ALL
+                    SELECT 'Papel higiênico' UNION ALL
+                    SELECT 'Milho' UNION ALL
+                    SELECT 'Sardinha' UNION ALL
+                    SELECT 'Ervilha' UNION ALL
+                    SELECT 'Óleo de soja' UNION ALL
+                    SELECT 'Margarina' UNION ALL
+                    SELECT 'Leite em pó' UNION ALL
+                    SELECT 'Leite longa vida'
+                ) AS novos_valores
+                WHERE NOT EXISTS (SELECT id FROM tipo_produto)
+                """;
+        runStatementData(sql, nomeTabela);
+    }
+
     private void runStatementTabela(String sql, String nomeTabela) throws SQLException {
         try (PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.executeUpdate();
             System.out.println("[Mariadb] Tabela " + nomeTabela + " criada com sucesso ou já existente.");
         } catch (SQLException e) {
             System.err.println("[Mariadb] Erro ao criar tabela " + nomeTabela + " no: " + e.getMessage());
@@ -62,7 +96,7 @@ public class mariadbCreateTable implements ICreateTable {
 
     private void runStatementData(String sql, String nomeTabela) throws SQLException {
         try (PreparedStatement ps = connection.prepareStatement(sql)) {
-            System.out.println(ps.executeUpdate());
+            ps.executeUpdate();
             System.out.println("[Mariadb] Dado inserido na Tabela " + nomeTabela + " com sucesso.");
         } catch (SQLException e) {
             System.err.println("[Mariadb] Erro ao inserir na tabela " + nomeTabela + " no: " + e.getMessage());
