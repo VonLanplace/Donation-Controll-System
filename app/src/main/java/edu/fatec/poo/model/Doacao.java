7428ae06751cbee7b13715d6c9c83c0ded2e099b
Lucas Sartorelli <lucaspmsartorelli@gmail.com>
2026-05-15 12:04:12 -0300

    feature: class doacao

---
+++ app/src/main/java/edu/fatec/poo/model/Doacao.java
@@ -0,0 +1,32 @@
+package edu.fatec.poo.model;
+
+import edu.fatec.poo.model.produto.Produto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Doacao implements IEntity {
+    private Long id;
+    private String nomeDoador;
+    private LocalDate data;
+    private Usuario cadastrante;
+    private List<Produto> produtos;
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
