a1a0b4bd1a728a448bb95db542e78036ce36329d
Lucas Sartorelli <lucaspmsartorelli@gmail.com>
2026-09-19 11:23:10 -0300

    feat: added Controller and page for Poduto Listar

---
+++ src/main/java/com/vonlanplace/doacao/produto/ProdutoController.java
@@ -0,0 +1,25 @@
+package com.vonlanplace.doacao.produto;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/produto")
+@Tag(name = "Produto", description = "Gerenciamento de produtos doáveis")
+public class ProdutoController {
+
+    @Autowired
+    private ProdutoService produtoService;
+
+    @GetMapping({"", "/", "/listar"})
+    @Operation(summary = "Listar todos os produtos cadastrados")
+    public String listarProdutos(Model model) {
+        model.addAttribute("produtos", produtoService.findAll());
+        return "produto/listar";
+    }
+}
